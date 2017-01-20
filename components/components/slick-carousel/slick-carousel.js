f992cde63ee1b301f87280ef1be5cfcce887ee15
Vlad@CFU <pereskokow@gmail.com>
2017-01-20 16:00:16 +0300

    carousel: added fade to title

--- components/components/slick-carousel/slick-carousel.js
+++ components/components/slick-carousel/slick-carousel.js
@@ -3,11 +3,12 @@
     attach: function (context) {
       $('.slick__slider', context).on('init', function (e, slick, currentSlide) {
         if (e.handled !== true) {
+          var $current_slide = $(slick.$slides[slick.currentSlide])
+          var current_slide_label = $current_slide.find('.views-field-title a').html();
 
           var $nav_container = $('<div class="slick__controll-panel"></div>')
               .appendTo('.slick')
-              .append('<h3 class="slick__fade-title">TEST</h3>')
-          ;
+              .append('<h3 class="slick__fade-title">' + current_slide_label + '</h3>');
           
           $('.slick-dots').appendTo($nav_container);
           $('.slick__arrow').appendTo($nav_container);
@@ -17,9 +18,18 @@
       });
 
       $('.slick__slider', context).on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+        if (currentSlide === nextSlide) {
+          return;
+        }
+        
         var current_slide_label = $(slick.$slides[nextSlide]).find('.views-field-title a').html();
 
-        $('.slick h3').html(current_slide_label);
+        var $title = $('.slick .slick__fade-title')
+        $title.fadeOut(250, function () {
+          $('.slick .slick__fade-title').html(current_slide_label);
+          $title.fadeIn(250);
+        })
+
       });
     }
   }
