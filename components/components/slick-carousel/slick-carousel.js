a26c591b0b2744117d6895843ddc202a35c875fb
Vlad@CFU <pereskokow@gmail.com>
2017-01-24 08:45:55 +0300

    slick-carousel is more unique

--- components/components/slick-carousel/slick-carousel.js
+++ components/components/slick-carousel/slick-carousel.js
@@ -1,32 +1,35 @@
 (function ($) {
   Drupal.behaviors.frontCarousel = {
     attach: function (context) {
-      $('.slick__slider', context).on('init', function (e, slick, currentSlide) {
+      var selector = '#block-views-block-testovaa-karusel-2-block-1';
+      var $carousel = $(selector);
+      
+      $(selector, context).on('init', function (e, slick, currentSlide) {
         if (e.handled !== true) {
           var $current_slide = $(slick.$slides[slick.currentSlide])
           var current_slide_label = $current_slide.find('.views-field-title a').html();
 
           var $nav_container = $('<div class="slick__controll-panel"></div>')
-              .appendTo('.slick')
+              .appendTo($carousel)
               .append('<h3 class="slick__fade-title">' + current_slide_label + '</h3>');
           
-          $('.slick-dots').appendTo($nav_container);
-          $('.slick__arrow').appendTo($nav_container);
+          $carousel.find('.slick-dots').appendTo($nav_container);
+          $carousel.find('.slick__arrow').appendTo($nav_container);
           
           e.handled = true;
         }
       });
 
-      $('.slick__slider', context).on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+      $(selector, context).on('beforeChange', function (e, slick, currentSlide, nextSlide) {
         if (currentSlide === nextSlide) {
           return;
         }
         
         var current_slide_label = $(slick.$slides[nextSlide]).find('.views-field-title a').html();
 
-        var $title = $('.slick .slick__fade-title')
+        var $title = $carousel.find('.slick__fade-title')
         $title.fadeOut(250, function () {
-          $('.slick .slick__fade-title').html(current_slide_label);
+          $title.html(current_slide_label);
           $title.fadeIn(250);
         })
 
