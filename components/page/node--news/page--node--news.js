69e85ad610d078bc15e0be722cee6d58e0846f50
Vlad@CFU <pereskokow@gmail.com>
2017-03-27 15:40:07 +0300

    some fixes

--- components/page/node--news/page--node--news.js
+++ components/page/node--news/page--node--news.js
@@ -1,6 +1,13 @@
 (function ($) {
-  $('.field--name-field-image-gallery .field__items').slick({
-    variableWidth: true,
-    slidesToScroll: 3
+  // $('.field--name-field-image-gallery .field__items').slick({
+  //   variableWidth: true,
+  //   slidesToScroll: 3
+  // });
+
+  var $gallery_container = $('.field--name-field-image-gallery > .field__items');
+
+  var msnry = new Masonry($gallery_container, {
+    itemSelector: '.field__items',
+    columnWidth: 100
   });
 })(jQuery, Drupal, drupalSettings);
