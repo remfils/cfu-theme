e907daecf2a939e48af50e8eec1343a8054b544e
Vlad@CFU <pereskokow@gmail.com>
2017-03-29 10:07:07 +0300

    news page design fixes

--- components/page/node--news/page--node--news.js
+++ components/page/node--news/page--node--news.js
@@ -1,13 +1,10 @@
 (function ($) {
-  // $('.field--name-field-image-gallery .field__items').slick({
-  //   variableWidth: true,
-  //   slidesToScroll: 3
-  // });
-
   var $gallery_container = $('.field--name-field-image-gallery > .field__items');
 
   var msnry = new Masonry($gallery_container, {
     itemSelector: '.field__items',
     columnWidth: 100
   });
+
+  $('p img.align-right').unwrap();
 })(jQuery, Drupal, drupalSettings);
