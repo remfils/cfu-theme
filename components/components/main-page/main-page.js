4112f3b9610ad2061bb12557fb32414154de3137
Vlad@CFU <pereskokow@gmail.com>
2017-03-31 10:25:38 +0300

    removed news more deprecated functionality

--- components/components/main-page/main-page.js
+++ components/components/main-page/main-page.js
@@ -1,15 +1,3 @@
 (function($, Drupal, drupalSetttings) {
-  // more news display block
-
-  var $more_link = $('.news-view .more-link');
-
-  $more_link.click(function() {
-    var $news_veiw = $('.news-view .view-content');
-    var h = $news_veiw.height();
-
-    $news_veiw.animate({height: h * 2}, 500);
-
-    $more_link.unbind('click')
-      .addClass('opened');
-  });
+  
 })(jQuery, Drupal, drupalSettings);
