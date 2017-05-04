77ac6b111b616e4ae983401ce13d3c1d97627275
Vlad@CFU <pereskokow@gmail.com>
2017-05-04 14:27:34 +0300

    masonry works

--- components/page/node--news/page--node--news.js
+++ components/page/node--news/page--node--news.js
@@ -1,9 +1,10 @@
 (function ($) {
   var $gallery_container = $('.field--name-field-image-gallery > .field__items');
 
-  var masonry = new Masonry($gallery_container, {
-    itemSelector: '.field__items',
-    columnWidth: '.field--name-field-image-gallery .field__items'
+  $gallery_container.masonry({
+    itemSelector: '.field__item',
+    columnWidth: $gallery_container.find('.field__item').get(0),
+    gutter: 5
   });
 
   $('p img.align-right').unwrap();
